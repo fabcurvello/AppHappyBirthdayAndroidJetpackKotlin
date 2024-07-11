d178165c7b4ae6806034180342891e70fcf5f759
Fabricio Curvello <fabricio.desenvolvedor@gmail.com>
2024-07-11 18:29:24 -0300

    Commit inicial

---
+++ app/src/main/java/br/com/fabriciocurvello/happybirthday/MainActivity.kt
@@ -0,0 +1,50 @@
+package br.com.fabriciocurvello.happybirthday
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import br.com.fabriciocurvello.happybirthday.ui.theme.HappyBirthdayTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HappyBirthdayTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    GreetingText(
+                        message = "Lambisgóia",
+                        modifier = Modifier.padding(innerPadding),
+                        from = "Godofredo"
+                    )
+                }
+            }
+        }
+    }
+
+}
+
+@Composable
+fun GreetingText(message: String, from:String, modifier: Modifier = Modifier) {
+    Text(text = message, fontSize = 100.sp, lineHeight = 116.sp)
+    Text(text = from, fontSize = 36.sp)
+}
+
+@Preview(showBackground = true,
+    showSystemUi = true,
+    name = "Meu Preview")
+@Composable
+fun GreetingPreview() {
+    HappyBirthdayTheme {
+        GreetingText(message = "Feliz aniversário Isa!", from = "Carlos")
+    }
+}
